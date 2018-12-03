42627db51731b50b84fbc0d97924012fa2a5c6b7
chenup <rexclq@gmail.com>
2018-12-03 13:10:57 +0800

    inital commit

---
+++ ta/.hello_world_ta.o.d
@@ -0,0 +1,17 @@
+hello_world_ta.o: hello_world_ta.c \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_internal_api.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_api.h \
+ /home/rex/devel/optee/toolchains/aarch64/lib/gcc/aarch64-linux-gnu/6.2.1/include/stddef.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/compiler.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_defines.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_types.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/stdint.h \
+ /home/rex/devel/optee/toolchains/aarch64/lib/gcc/aarch64-linux-gnu/6.2.1/include/stdbool.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_ta_api.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_internal_api_extensions.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/trace.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/trace_levels.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/stdio.h \
+ /home/rex/devel/optee/toolchains/aarch64/lib/gcc/aarch64-linux-gnu/6.2.1/include/stdarg.h \
+ /home/rex/devel/optee/optee_os/out/arm/export-ta_arm64/include/tee_api_defines_extensions.h \
+ include/hello_world_ta.h
